f877142b00a398dac457666dc92feb7bc05576b3
aaronjtonner <tonneraaron@gmail.com>
2023-05-03 14:12:58 -0600

    pixel

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,5 +36,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-facebook-pixel`,
+      options: {
+        pixelId: "145078160490439",
+      },
+    },
   ],
 }
